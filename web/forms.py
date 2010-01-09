bf7a2c99ebadac1ea8e2675bffb5675a3ce44f7b
Frederic Descamps <fred@delvaux.maladree.be>
2010-01-09 21:41:44 +0100

    some files were not added by the commit in eclipse

---
+++ web/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.admin import widgets  
+from banquise.web.models import Customer, Host, Package, ServerPackages, Contract
+
+class ContractForm(forms.ModelForm):
+    start_date = forms.DateField(('%Y-%m-%d',),   
+        widget=forms.DateTimeInput(format='%Y-%m-%d', attrs={
+            'class':'input',
+            'readonly':'readonly',
+            'size':'10'
+        })
+    )
+    end_date = forms.DateField(('%Y-%m-%d',),   
+        widget=forms.DateTimeInput(format='%Y-%m-%d', attrs={
+            'class':'input',
+            'readonly':'readonly',
+            'size':'10'
+        })
+    )
+    class Meta:
+        model = Contract
+        fields = ('start_date','end_date')
+
+class PackageForm(forms.ModelForm):
+    name = forms.CharField(required=False)
+    arch = forms.CharField(required=False)
+    version = forms.CharField(required=False)
+    release = forms.CharField(required=False)
+    class Meta:
+        model = Package
+        
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
---
+++ web/migrations/0007_skip_package.py
@@ -0,0 +1,71 @@
+
+from south.db import db
+from django.db import models
+from banquise.web.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding field 'Customer.email'
+        db.add_column('web_customer', 'email', orm['web.customer:email'])
+        
+        # Adding field 'Customer.contact'
+        db.add_column('web_customer', 'contact', orm['web.customer:contact'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'Customer.email'
+        db.delete_column('web_customer', 'email')
+        
+        # Deleting field 'Customer.contact'
+        db.delete_column('web_customer', 'contact')
+        
+    
+    
+    models = {
+        'web.contract': {
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['web.Customer']"}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'hosts': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['web.Host']", 'symmetrical': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'license': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        },
+        'web.customer': {
+            'contact': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '150'})
+        },
+        'web.host': {
+            'hash': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'public_ip': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'release': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'web.package': {
+            'Meta': {'unique_together': "(('name', 'arch', 'version', 'release'),)"},
+            'arch': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'release': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'web.serverpackages': {
+            'date_available': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'date_installed': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'host': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['web.Host']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'package': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['web.Package']"}),
+            'package_installed': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'package_skipped': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'to_install': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+        }
+    }
+    
+    complete_apps = ['web']
