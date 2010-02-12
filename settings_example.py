f583878b12681c9a0e182b5704344076a21e6287
Frederic Descamps <lefred@inuits.be>
2010-02-12 14:10:53 +0100

    changing the conf Signed-off-by: Frederic Descamps <lefred@inuits.be>

--- settings_example.py
+++ settings_example.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/fred/workspace/banquise/web/media/'
+MEDIA_ROOT = '/var/www/banquise/web/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -70,7 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'home/fred/code/banquise/templates'
+    '/var/www/banquise/templates'
 )
 
 INSTALLED_APPS = (
